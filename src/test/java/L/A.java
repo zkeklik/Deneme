fae9c342558707061cc6141ec9aa3f206212b8d9
zkeklik <zlyhkeklik@gmail.com>
2021-12-24 22:35:47 +0100

    conflict

--- src/test/java/L/A.java
+++ src/test/java/L/A.java
@@ -5,5 +5,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("cat and dog");
         System.out.println("door");
+        System.out.println("");
     }
 }
